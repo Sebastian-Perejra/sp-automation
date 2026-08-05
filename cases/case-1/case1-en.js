88c695ae5ee1ebf97357e47811d81fa6eeb842cf
Sebastian-Perejra <sebast.perejra@gmail.com>
2026-08-05 11:06:39 +0300

    Update case1-en.js

--- cases/case-1/case1-en.js
+++ cases/case-1/case1-en.js
@@ -1,1 +1,201 @@
-
+const bankRows = [
+  [
+    "03.07.2026",
+    "CARD*4821 POS 19:44 NOVUS KYIV UA MCC5411 REF 884521",
+    "-1 286,40",
+    "NOVUS",
+    "Groceries",
+    "ok"
+  ],
+  [
+    "03.07.2026",
+    "UAH KOM 0.00 / PEREKAZ Z KARTY 5168****1047 VID IVANENKO O.O.",
+    "+12 500,00",
+    "Іваненко О.О.",
+    "Incoming payment",
+    "ok"
+  ],
+  [
+    "04.07.2026",
+    "MONO PAY*GOOGLE ADS 874201 IE DUBLIN IRL 04/07",
+    "-2 940,18",
+    "Google Ads",
+    "Advertising",
+    "ok"
+  ],
+  [
+    "04.07.2026",
+    "PLATIZH ZA RAH 1048-26 TOV VEKTOR SERVIS BEZ PDV",
+    "-7 200,00",
+    "ТОВ «Вектор Сервіс»",
+    "Services",
+    "ok"
+  ],
+  [
+    "05.07.2026",
+    "ATM CASH WD 005771 KYIV PR-T PEREMOHY 12 KOM 80.00",
+    "-4 000,00",
+    "Банкомат",
+    "Cash withdrawal",
+    "ok"
+  ],
+  [
+    "05.07.2026",
+    "NOVA POSHTA 02041 KYIV TERMINAL 330192 MCC4215",
+    "-486,00",
+    "Нова пошта",
+    "Logistics",
+    "ok"
+  ],
+  [
+    "06.07.2026",
+    "EPICENTR K POS 8871 BUD MATERIALY CHECK 001947",
+    "-3 742,90",
+    "Епіцентр",
+    "Operating supplies",
+    "ok"
+  ],
+  [
+    "06.07.2026",
+    "P2P CREDIT 4149****7720 KOVALENKO SERHII D.",
+    "+8 000,00",
+    "Коваленко С.Д.",
+    "Incoming payment",
+    "ok"
+  ],
+  [
+    "07.07.2026",
+    "FOP MELNYK O.V. RAH 708 OPлата za druk etyketok",
+    "-5 600,00",
+    "ФОП Мельник О.В.",
+    "Printing",
+    "ok"
+  ],
+  [
+    "07.07.2026",
+    "UBER *TRIP HELP.UBER.COM NL 07-07 AUTH 102944",
+    "-318,72",
+    "Uber",
+    "Transport",
+    "ok"
+  ],
+  [
+    "08.07.2026",
+    "VAT PAYMENT 2 KV 2026 EDPR 3011400000 PRYZN 101",
+    "-18 760,00",
+    "Tax Authority",
+    "Taxes",
+    "ok"
+  ],
+  [
+    "08.07.2026",
+    "ROZETKA UA KYIV ORDER RZK-4938172 OFFICE MOUSE",
+    "-1 149,00",
+    "Rozetka",
+    "Office expenses",
+    "ok"
+  ],
+  [
+    "09.07.2026",
+    "APPLE.COM/BILL 800-275-2273 IRL ICLOUD+ 200GB",
+    "-149,00",
+    "Apple",
+    "Subscriptions",
+    "ok"
+  ],
+  [
+    "09.07.2026",
+    "TOV MHP POSTACHANNYA ZA DOG 14/26 RAH 553",
+    "+34 980,00",
+    "ТОВ «МХП»",
+    "Client payment",
+    "ok"
+  ],
+  [
+    "10.07.2026",
+    "WOG A-95 31.42L ST 083 KYIV OBL TERM 774912",
+    "-1 842,65",
+    "WOG",
+    "Fuel",
+    "ok"
+  ],
+  [
+    "10.07.2026",
+    "FOP PETRENKO A.A. AVANS ZA DIZAYN MAKETIV",
+    "-3 000,00",
+    "ФОП Петренко А.А.",
+    "Design",
+    "ok"
+  ],
+  [
+    "11.07.2026",
+    "SILPO 118 POS 2881 MCC 5411 KYIV 11.07.26",
+    "-947,31",
+    "Сільпо",
+    "Groceries",
+    "ok"
+  ],
+  [
+    "11.07.2026",
+    "PRIVAT24 KOMISIYA ZA RKO TARIF BUSINESS",
+    "-199,00",
+    "ПриватБанк",
+    "Bank fees",
+    "ok"
+  ],
+  [
+    "12.07.2026",
+    "TOV NOVA LINIYA CHECK 62287 INSTRUMENTY SKLAD",
+    "-2 386,50",
+    "Нова Лінія",
+    "Operating supplies",
+    "ok"
+  ],
+  [
+    "12.07.2026",
+    "PEREKAZ VID TOV ALFA TRADE RAH 204 POSLUGY",
+    "+21 400,00",
+    "ТОВ «Альфа Трейд»",
+    "Client payment",
+    "ok"
+  ],
+  [
+    "13.07.2026",
+    "MICROSOFT*SUBSCRIPTION MSFT IE M365 BUSINESS",
+    "-512,40",
+    "Microsoft",
+    "Subscriptions",
+    "ok"
+  ],
+  [
+    "13.07.2026",
+    "BOLT.EU/R/240713 KYIV UA RIDE 229184",
+    "-204,00",
+    "Bolt",
+    "Transport",
+    "ok"
+  ],
+  [
+    "14.07.2026",
+    "PAYMENT TO FOP BONDARENKO V.V. HOSTING 1 YEAR",
+    "-2 100,00",
+    "ФОП Бондаренко В.В.",
+    "Hosting",
+    "ok"
+  ],
+  [
+    "14.07.2026",
+    "CREDIT P2P 5355****8802 SHEVCHENKO N.M.",
+    "+5 000,00",
+    "Шевченко Н.М.",
+    "Incoming payment",
+    "ok"
+  ],
+  [
+    "15.07.2026",
+    "KYIVSTAR B2B INV 726193 TEL +380980746698",
+    "-450,00",
+    "Kyivstar",
+    "Telecommunications",
+    "ok"
+  ]

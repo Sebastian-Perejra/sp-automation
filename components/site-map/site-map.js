d29b8dd63eedd910728e7c999074dcc026a176d5
Sebastian-Perejra <sebast.perejra@gmail.com>
2026-08-20 14:54:58 +0300

    Implement site map trigger click logging
    Add event listener to site map trigger for logging.

--- components/site-map/site-map.js
+++ components/site-map/site-map.js
@@ -1,1 +1,21 @@
+(() => {
+  const trigger =
+    document.querySelector(
+      "[data-site-map-trigger]"
+    );
 
+  if (!trigger) {
+    return;
+  }
+
+  trigger.addEventListener(
+    "click",
+    () => {
+      console.log(
+        "Site map:",
+        document.documentElement.dataset.sitePage,
+        document.documentElement.dataset.siteLanguage
+      );
+    }
+  );
+})();
